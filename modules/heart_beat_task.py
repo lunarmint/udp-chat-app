2adfa06c6b3c91dfa7cc78392c0e6364d6c643c5
MINH KHANG TRAN <ktran2@oswego.edu>
2022-05-13 03:40:41 -0400

    Create a thread to check heart beat

---
+++ modules/heart_beat_task.py
@@ -0,0 +1,22 @@
+from socket import socket
+from threading import Thread, Event
+import typer
+from packets.heartbeat import Heartbeat
+
+
+class HeartBeatTask(Thread):
+    def __init__(self, sock: socket, address: str, event: Event, delay: int, key: str):
+        Thread.__init__(self)
+        self.sock = sock
+        self.address = address
+        self.stopped = event
+        self.delay = delay
+        self.key = key
+
+    def run(self):
+        index = 1
+        while not self.stopped.wait(self.delay):
+            heartbeat_packet = Heartbeat(index, self.key)
+            self.sock.sendto(heartbeat_packet, self.address)
+            index += 1
+        typer.echo(typer.style("DISCONNECTED!", fg=typer.colors.RED))
