2968682b5478daac56fc553dcb568a34ec470a98
MINH KHANG TRAN <ktran2@oswego.edu>
2022-05-13 03:40:28 -0400

    Create a thread to receive message

---
+++ modules/receive_message_task.py
@@ -0,0 +1,29 @@
+from socket import socket
+from threading import Thread, Event
+import typer
+from packets.opcode import OpCode
+from utils import xor_decode
+
+MAX_LENGTH = 1024
+
+
+class ReceiveMsgTask(Thread):
+    def __init__(self, sock: socket, event: Event, key: str):
+        Thread.__init__(self)
+        self.sock = sock
+        self.stopped = event
+        self.key = key
+
+    def run(self):
+        while not self.stopped:
+            data, _ = self.sock.recvfrom(MAX_LENGTH)
+            data = xor_decode(data, self.key, as_bytes=False)
+            opcode = int(data[0])
+            if opcode == OpCode.Msg.value:
+                message = str(data[1:]).split("\t")
+                user_name, msg = message[0], message[1]
+                typer.echo(user_name + ": " + msg)
+            elif opcode == OpCode.Error.value:
+                typer.echo(typer.style(data[1:], fg=typer.colors.RED))
+            else:
+                typer.echo(typer.style("Receive unknown data: " + data, fg=typer.colors.RED))
