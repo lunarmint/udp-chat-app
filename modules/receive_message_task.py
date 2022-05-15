558118d66d1630c0ee580d845859bf0beb4ed667
MINH KHANG TRAN <ktran2@oswego.edu>
2022-05-15 12:38:18 -0400

    Update the receive message thread

--- modules/receive_message_task.py
+++ modules/receive_message_task.py
@@ -8,22 +8,31 @@ MAX_LENGTH = 1024
 
 
 class ReceiveMsgTask(Thread):
-    def __init__(self, sock: socket, event: Event, key: str):
+    def __init__(self, sock: socket, event: Event, delay: int, key: str):
         Thread.__init__(self)
         self.sock = sock
         self.stopped = event
+        self.delay = delay
         self.key = key
 
     def run(self):
-        while not self.stopped.wait(0):
-            data, _ = self.sock.recvfrom(MAX_LENGTH)
-            data = xor_decode(data, self.key, as_bytes=False)
-            opcode = int(data[0])
-            if opcode == OpCode.Msg.value:
-                message = str(data[1:]).split("\t")
-                user_name, msg = message[0], message[1]
-                typer.echo(user_name + ": " + msg)
-            elif opcode == OpCode.Error.value:
-                typer.echo(typer.style(data[1:], fg=typer.colors.RED))
-            else:
-                typer.echo(typer.style("Receive unknown data: " + data, fg=typer.colors.RED))
+        while not self.stopped.wait(self.delay):
+            try:
+                data, _ = self.sock.recvfrom(MAX_LENGTH)
+                data = xor_decode(data, self.key, as_bytes=False)
+                opcode = int(data[0])
+                if opcode == OpCode.Msg.value:
+                    message = str(data[1:])
+                    typer.echo(message)
+                elif opcode == OpCode.Green.value:
+                    message = str(data[1:])
+                    typer.echo(typer.style(message, fg=typer.colors.GREEN))
+                elif opcode == OpCode.Yellow.value:
+                    message = str(data[1:])
+                    typer.echo(typer.style(message, fg=typer.colors.YELLOW))
+                elif opcode == OpCode.Error.value:
+                    typer.echo(typer.style(data[1:], fg=typer.colors.RED))
+                else:
+                    typer.echo(typer.style("Receive unknown data: " + data, fg=typer.colors.RED))
+            except:
+                break
